39dad06e2a278c9c757fcb56968d6139f18641bb
Coachbre <brekonan1@gmail.com>
2021-03-10 12:41:33 -0500

    added filter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,14 +4,31 @@ import { PostList } from "./feed/PostList.js"
 import { NavBar } from "./nav/NavBar.js"
 import { Footer } from "./nav/Footer.js"
 
-// *event listener
+
+// *event listener goes here ********
 const applicationElement = document.querySelector(".giffygram");
-applicationElement.addEventListener("click", event => {
-	if (event.target.id === "directMessageIcon")
+applicationElement.addEventListener("change", event => {
+	if (event.target.id === "yearSelection")
     {
-		console.log("Wanna send a message?")
+		const yearAsNumber = parseInt(event.target.value)
+		console.log(`user wants to see posts since ${yearAsNumber}`)
+		// invoke a filter function passing the year as an argument
+		showFilteredPosts(yearAsNumber);
 	}
 })
+
+const showFilteredPosts = (year) => {
+	//get a copy of the post collection
+	const epoch = Date.parse(`01/01/${year}`);
+	//filter the data
+	const filteredData = usePostCollection().filter(singlePost => {
+	  if (singlePost.timestamp >= epoch) {
+		return singlePost
+	  }
+	})
+	const postElement = document.querySelector(".postList");
+	postElement.innerHTML = PostList(filteredData);
+  }
 
 
 
